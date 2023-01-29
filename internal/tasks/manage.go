81237f3d80d0969334d46b039a0dc3de9bb9b978
Christian Deacon <gamemann@gflclan.com>
2023-01-29 16:25:01 -0600

    Add tasks module.

---
+++ internal/tasks/manage.go
@@ -0,0 +1,21 @@
+package tasks
+
+import (
+	"bestmods/tasks/internal/config"
+	"fmt"
+
+	"github.com/gamemann/Rust-Auto-Wipe/pkg/chttp"
+)
+
+func Exec(task config.Task, debug int) {
+	_, _, err := chttp.SendHTTPReq(task.URL, task.Auth, task.Method, nil)
+
+	if err != nil {
+		fmt.Printf("Error executing task '%s' with method '%s' and auth '%s'.\n", task.URL, task.Method, task.Auth)
+		fmt.Println(err)
+	}
+
+	if debug > 1 {
+		fmt.Printf("Task '%s' with method '%s' executed.", task.URL, task.Method)
+	}
+}
